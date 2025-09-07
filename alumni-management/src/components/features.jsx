20811a425d7e1c6455cff81fc4d0db7459da1b4d
manmeet <manmeetchahal231@gmail.com>
2025-09-07 11:59:50 +0530

    serch_background

--- alumni-management/src/components/features.jsx
+++ alumni-management/src/components/features.jsx
@@ -5,8 +5,21 @@ import bgImage from "../assets/images/feaback.jpg";
 const Features = () => {
   return (
     <div className="features">
+        <div className="card-container">
+        <div className="feature-card">
+          <h2>Networking</h2>
+          <p>Connect with alumni across the globe.</p>
+        </div>
+        <div className="feature-card">
+          <h2>Opportunities</h2>
+          <p>Discover jobs, internships, and mentorships.</p>
+        </div>
+        <div className="feature-card">
+          <h2>Events</h2>
+          <p>Stay updated with reunions and webinars.</p>
+        </div>
+        </div>
     </div>
-    
   );
 };
 
